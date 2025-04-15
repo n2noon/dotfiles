fc68bde6c874cd4f0cde0cd3b0145ade652089e0
echostain <melyil153@gmail.com>
2025-04-15 03:55:13 +0100

    Add .config/nvim/lua/plugins/apple.lua Add .config/nvim/lua/plugins/gitsigns.lua Add .config/nvim/lua/plugins/llm.lua

---
+++ private_dot_config/nvim/lua/plugins/llm.lua
@@ -0,0 +1,11 @@
+return {
+  "ggml-org/llama.vim",
+  init = function()
+    vim.g.llama_config = {
+      -- auto_fim = false,
+      keymap_accept_full = "<C-S>",
+      keymap_accept_line = "<Right>",
+      show_info = false,
+    }
+  end,
+}
