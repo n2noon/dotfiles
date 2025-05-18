8d9afee1ba4cecf4b9f4bd373dad789c6659f74c
echostain <melyil153@gmail.com>
2025-05-18 19:24:39 +0100

    Add .config/fish/conf.d/git-alias.fish Add .config/fish/conf.d/source_once.fish

---
+++ private_dot_config/private_fish/conf.d/git-alias.fish
@@ -0,0 +1,12 @@
+abbr -a g git
+
+## Commit ##
+abbr -a gc git commit
+abbr -a gcm git commit --message
+
+abbr -a ga git add
+abbr -a gs git status
+abbr -a gl git log
+abbr -a gp git pull
+abbr -a gf git fetch
+abbr -a gr git rebase
---
+++ private_dot_config/private_fish/conf.d/source_once.fish
@@ -0,0 +1,38 @@
+# This is to lazy load certain slow things so fish stays snappy
+if set -q SOURCED_ONCE
+return
+end
+
+set -gx SOURCED_ONCE
+
+source $wd/os.fish
+
+if type -q chezmoi
+    chezmoi completion fish | source
+end
+
+if type -q zoxide
+    zoxide init fish | source
+end
+
+if type -q fzf
+    fzf --fish | source
+end
+
+if type -q orbctl
+    orbctl completion fish | source
+end
+
+if type -q brew
+    fish_add_path /opt/homebrew/bin/
+    fish_add_path /opt/homebrew/sbin/
+    fish_add_path /opt/homebrew/opt/openjdk/bin/
+end
+
+if type -q cargo
+    fish_add_path ~/.cargo/bin/
+end
+
+### Path ###
+fish_add_path ~/.bin
+fish_add_path ~/.local/bin
